67700e6762156413b058c3bc208f4f2cd1e25858
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2024-10-24 19:04:08 -0400

    Add data

--- all_data.py
+++ all_data.py
@@ -7,7 +7,11 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 import pandas as pd
 from time import sleep
 
-YEAR = 2023
+#https://www.wunderground.com/history/daily/ca/quebec-city/CYQB/date/2023-12-28
+#https://www.wunderground.com/weather/ca/chicoutimi
+# TODO: scrap https://www.wunderground.com/history/daily/ca/la-baie/CYBG/date/2023-%M-%D 
+
+YEAR = 2020
 agenda = {"1":31,
           "2":28,
           "3":31,
@@ -21,37 +25,34 @@ agenda = {"1":31,
           "11":30,
           "12":31
 }
-# Configurer le chemin vers geckodriver (s'il est dans votre PATH, ce n'est pas nécessaire)
+
 service = FirefoxService(executable_path='/usr/local/bin/geckodriver')
-HEADER = ["Time", "Temperature" ,"Dew Point", "Humidity", "Wind", "Wind Speed", "Wind Gust", "Pressure Precip.", "Condition"]
-# Démarrer le navigateur Firefox
+HEADER = ["Time", "Temperature" ,"Dew Point", "Humidity", "Wind", "Wind Speed", "Wind Gust", "Pressure", "Precip.", "Condition"]
 driver = webdriver.Firefox(service=service)
 data = []
 
 def scrape_weather_data(year, month, day):
     # Construire l'URL
     url = f"https://www.wunderground.com/history/daily/ca/la-baie/CYBG/date/{year}-{month:02d}-{day:02d}"
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(5)
     driver.get(url)
-    sleep(2)  # Attendre quelques secondes pour que le contenu dynamique se charge
+    if month == 1 and day == 1  :
+        sleep(2)  # Attendre quelques secondes pour que le contenu dynamique se charge
     print("WAITED")
-
-    sleep(2)
-        # Rechercher la table (ajuster le sélecteur si nécessaire)
-        
+    sleep(0.5)
     table = driver.find_element(By.XPATH, '//table[@class="mat-table cdk-table mat-sort ng-star-inserted"]')
     rows = table.find_elements(By.TAG_NAME, 'tr')
 
     
     i = 0
-    print(f"[DEBUG] {len(rows)=}")
+    print(f"[INFO] {len(rows)} rows détécté")
     for row in rows:
         cols = row.find_elements(By.TAG_NAME, 'td')
         if cols:
             
             tmp_data = [col.text for col in cols]
             tmp_data[0] = f"{year}/{month}/{day} {tmp_data[0]}"
-            print(f"[DEBUG] {i}/{len(rows)} {tmp_data}")
+            #print(f"[INFO] {i}/{len(rows)} {tmp_data} DONE")
             data.append(tmp_data)
         elif i>len(rows)-11:
             break
@@ -60,18 +61,24 @@ def scrape_weather_data(year, month, day):
 
 for month in agenda:
     for day in range(1,agenda[month] + 1):
-        print(f"[DEBUG] Scraping date: {day}/{month}/{YEAR}")
+        print(f"[INFO] Scraping date: {day}/{month}/{YEAR}")
         scrape_weather_data(YEAR, int(month), day)
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.CONTROL + 't')
 
-print(f"DATA {data=}")
 
-
-df = pd.DataFrame(data, columns=headers)
+df = pd.DataFrame(data, columns=HEADER)
 
 if df is not None:
-    print(df)
-    df.to_csv("weather_data_seleniumv2.csv", index=False)
+    # 1. Conversion des températures de Fahrenheit à Celsius
+    # Formule : (F - 32) * 5/9
+    df['Temperature (°C)'] = df['Temperature'].str.replace('°F', '').astype(float).apply(lambda f: (f - 32) * 5/9)
+    # 2. Conversion de la vitesse du vent de mph à m/s
+    # Formule : 1 mph = 0.44704 m/s
+    df['Wind Speed (m/s)'] = df['Wind Speed'].str.replace(' mph', '').astype(float) * 0.44704
+    # 3. Conversion de la pression de inHg à Pa
+    # Formule : 1 inHg = 3386.39 Pa
+    df['Pressure (Pa)'] = df['Pressure'].str.replace(' in', '').astype(float) * 3386.39
+    print(f"[INFO] Succesfull created DataFrame : \n{df.head}")
+    df.to_csv(f"WD__{YEAR}.csv", index=False)
 
-# Fermer le navigateur
 #driver.quit()
