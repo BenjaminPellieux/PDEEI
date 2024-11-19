72e849e1da6fe468a8e286631b732e4b49c1479c
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2024-11-19 13:05:19 -0500

    DEBUG source element

--- Jalon3_PELB28120100.py
+++ Jalon3_PELB28120100.py
@@ -28,31 +28,31 @@ from matplotlib.animation import FuncAnimation
 ##########################################
 
 # Animation
-FPS = 60  # Fréquence d'images par seconde pour l'animation
-GIF = True  # Si True, enregistre l'animation en GIF
+FPS: int = 60  # Fréquence d'images par seconde pour l'animation
+GIF: bool = True  # Si True, enregistre l'animation en GIF
 
 ###################################
 #        Données géométriques     #
 ###################################
 
-L = 0.30  # Longueur du mur (m)
-l = 3  # Largeur du mur (m)
-h = 2  # Hauteur du mur (m)
-S = h * l  # Surface du mur (m²)
+L: float = 0.30  # Longueur du mur (m)
+l: int = 3  # Largeur du mur (m)
+h: int = 2  # Hauteur du mur (m)
+S: int = h * l  # Surface du mur (m²)
 
 ###################################
 #       Données thermiques        #
 ###################################
 
 # Températures initiales et limites
-T_init = 12  # Température initiale dans le mur (°C)
-T_left = 25  # Température à la frontière gauche (°C) - Dirichlet
-T_cible = 18 # Température cible pour le mur de droite
+T_init: int = 12  # Température initiale dans le mur (°C)
+T_left: int = 15  # Température à la frontière gauche (°C) - Dirichlet
+T_cible: int = 18 # Température cible pour le mur de droite
 
 # Propriétés des matériaux
 k_beton, c_beton, p_beton = 1.5, 880, 2200  # Béton standard
-k_air, c_air, p_air, h_air= 0.026, 1004, 1.204, 20  # Air
-power_beton_conducteur = 3000  # Puissance thermique dans la couche chauffante (W)
+k_air, c_air, p_air, h_air = 0.026, 1004, 1.204, 20  # Air
+power_beton_conducteur: int = 200  # Puissance thermique dans la couche chauffante (W)
 
 ###################################
 #     Parametre de Simulation     #
@@ -61,29 +61,33 @@ power_beton_conducteur = 3000  # Puissance thermique dans la couche chauffante (
 # Distribution des couches dans le mur:  1/6 beton // 1/6 air // 1/20 beton conducteur et le reste de beton +- 4/6
 NVF_beton1, NVF_air, NVF_beton_conducteur, NVF_beton2 = 200, 500, 10, 1000  # Nombre de volumes finis
 
-NVF_tot = NVF_beton1 + NVF_air + NVF_beton_conducteur + NVF_beton2 
-comp_mur = [round(NVF_tot / 6), round(NVF_tot / 6), round(NVF_tot / 20)]
+NVF_tot: int = NVF_beton1 + NVF_air + NVF_beton_conducteur + NVF_beton2 
+comp_mur: list[float] = [round(NVF_tot / 6), round(NVF_tot / 6), round(NVF_tot / 20)]
 NVF_beton2 = NVF_tot - sum(comp_mur)
 comp_mur.append(NVF_beton2)
  
 # Propriétés thermiques par couche
-k_values = np.array([k_beton] * comp_mur[0] + 
+k_values: np.ndarray[float] = np.array(
+                    [k_beton] * comp_mur[0] + 
                     [k_air]   * comp_mur[1] + 
                     [k_beton] * comp_mur[2] + 
                     [k_beton] * comp_mur[3])
 
-c_values = np.array([c_beton] * comp_mur[0] + 
+c_values: np.ndarray[float] = np.array(
+                    [c_beton] * comp_mur[0] + 
                     [c_air]   * comp_mur[1] + 
                     [c_beton] * comp_mur[2] + 
                     [c_beton] * comp_mur[3])
 
-p_values = np.array([p_beton] * comp_mur[0] + 
+p_values: np.ndarray[float] = np.array(
+                    [p_beton] * comp_mur[0] + 
                     [p_air]   * comp_mur[1] + 
                     [p_beton] * comp_mur[2] + 
                     [p_beton] * comp_mur[3])
 
 # Dimensions des volumes finis
-dx_values = np.array([L / (6 * NVF_beton1)] * comp_mur[0] + 
+dx_values: np.ndarray[float] = np.array(
+                     [L / (6 * NVF_beton1)] * comp_mur[0] + 
                      [L / (6 * NVF_air)] * comp_mur[1] + 
                      [L / (20 * NVF_beton_conducteur)] * comp_mur[2] + 
                      [L / NVF_beton2] * comp_mur[3]
@@ -91,17 +95,18 @@ dx_values = np.array([L / (6 * NVF_beton1)] * comp_mur[0] +
 v_values = dx_values * S
 
 # Paramètres temporels
-heures = 48  # Durée de la simulation (heures)
-t_total = 3600 * heures  # Simulation sur X heures
-dt = 1800  # Intervalle de temps en secondes (30 minutes)
+HEURES: int = 96  # Durée de la simulation (heures)
+t_total: int = 3600 * HEURES  # Simulation sur X heures
+dt: int = 1800  # Intervalle de temps en secondes
 
-T_old = np.ones(NVF_tot) * T_init  # Température initiale
-T_new = np.copy(T_old)
-src = np.zeros(NVF_tot)
+T_old: np.ndarray[float] = np.ones(NVF_tot) * T_init  # Température initiale
+T_new: np.ndarray[float] = np.copy(T_old)
+src: np.ndarray[float] = np.zeros(NVF_tot)
 src[NVF_beton1 + NVF_air : NVF_beton1 + NVF_air + NVF_beton_conducteur] = power_beton_conducteur / NVF_beton_conducteur
 
 # Stocker toutes les températures pour chaque étape de temps
-all_temperatures = []
+all_source_status: list[bool] = []
+all_temperatures: list[np.ndarray] = []
 
 ##########################################
 #         Fonctions de simulation        #
@@ -124,7 +129,7 @@ def solve_mixte_neumann() -> np.ndarray:
         A[i, i - 1], A[i, i + 1] = -k_eff_lower, -k_eff_upper
         A[i, i] = k_eff_lower + k_eff_upper + (p_values[i] * c_values[i] * v_values[i]) / dt
         
-        if float(T_old[-1]) < T_cible:
+        if float(T_new[-1]) < T_cible:
             B[i] += src[i] * ((c_values[i] * p_values[i] * v_values[i]) / dt)
 
     # Conditions aux limites
@@ -147,8 +152,9 @@ def init_ani():
 
 def update_ani(frame):
     T_new = all_temperatures[frame]
+    source_status = all_source_status[frame]
     line.set_data(x, T_new)
-    time_text.set_text(f'Heure de simulation : {frame * dt / 3600:.2f}h \n Température intérieur {float(T_new[-1]):.2f}°C')
+    time_text.set_text(f" Heure de simulation : {frame * dt / 3600:.0f}h \n Température intérieur {float(T_new[-1]):.2f}°C\n Status de l'élement chuaffant: {'Activé' if source_status else 'Désactivé'}")
     return line, time_text
 
 
@@ -165,12 +171,16 @@ print(f"[INFO] Running Mixte - Neumann")
 
 print(f"[INFO] Durée en seconde {t_total}s")
 print(f"[INFO] Nombre d'éléments finis : {NVF_tot}")
+print(f"[INFO] Intervalle de temps en secondes {dt}s  {int(dt/60)} minutes")
+
 
 for _ in range(0, t_total, dt):
 
     for j in range(NVF_tot):
         T_old[j] = T_new[j] * ((c_values[j] * p_values[j] * v_values[j]) / dt)
     T_new = solve_mixte_neumann()
+    
+    all_source_status.append(float(T_new[-1]) < T_cible)
     all_temperatures.append(T_new.copy())
 
 ##########################################
@@ -186,12 +196,12 @@ ax.set_ylabel("Température (°C)")
 ax.set_title(f"Évolution de la température dans le mur en fonction du temps\n Temparture cible: {T_cible}°C")
 ax.legend(loc="upper left")
 
-time_text = ax.text(0.02, 0.85, '', transform=ax.transAxes, color="black", fontsize=12, ha="left")
+time_text = ax.text(0.00, 0.85, '', transform=ax.transAxes, color="black", fontsize=12, ha="left")
 
 ani = FuncAnimation(fig, update_ani, frames=len(all_temperatures), init_func=init_ani, blit=True, repeat=True)
 
 if GIF:
-    ani.save("Simulation_Jalon3.gif", writer='pillow', fps=FPS)
-    print("L'animation a été enregistrée sous le nom Simulation_Jalon3.gif")
+    ani.save(f"Simulation_Jalon3_{HEURES}H.gif", writer='pillow', fps=FPS)
+    print(f"L'animation a été enregistrée sous le nom Simulation_Jalon3_{HEURES}H.gif")
     
 plt.show()
