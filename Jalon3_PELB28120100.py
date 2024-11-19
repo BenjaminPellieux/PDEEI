f18652fcbac6fa3272f7e67560c5e41b530e2b07
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2024-11-19 14:48:51 -0500

    OPTI and new gif simulation with poser status

--- Jalon3_PELB28120100.py
+++ Jalon3_PELB28120100.py
@@ -59,7 +59,7 @@ power_beton_conducteur: int = 200  # Puissance thermique dans la couche chauffan
 ###################################
 
 # Distribution des couches dans le mur:  1/6 beton // 1/6 air // 1/20 beton conducteur et le reste de beton +- 4/6
-NVF_beton1, NVF_air, NVF_beton_conducteur, NVF_beton2 = 200, 500, 10, 1000  # Nombre de volumes finis
+NVF_beton1, NVF_air, NVF_beton_conducteur, NVF_beton2 = 800, 800, 100, 600  # Nombre de volumes finis
 
 NVF_tot: int = NVF_beton1 + NVF_air + NVF_beton_conducteur + NVF_beton2 
 comp_mur: list[float] = [round(NVF_tot / 6), round(NVF_tot / 6), round(NVF_tot / 20)]
@@ -123,8 +123,8 @@ def solve_mixte_neumann() -> np.ndarray:
     # Construction de la matrice et du vecteur B
     for i in range(1, NVF_tot - 1):
 
-        k_eff_lower = (((dx_values[i] * k_values[i] + dx_values[i - 1] * k_values[i - 1]) * S) / (dx_values[i] + dx_values[i - 1])) /  ((dx_values[i] + dx_values[i - 1]) / 2)
-        k_eff_upper = (((dx_values[i] * k_values[i] + dx_values[i + 1] * k_values[i + 1]) * S) / (dx_values[i] + dx_values[i + 1])) /  ((dx_values[i] + dx_values[i + 1]) / 2)
+        k_eff_lower: float = (((dx_values[i] * k_values[i] + dx_values[i - 1] * k_values[i - 1]) * S) / (dx_values[i] + dx_values[i - 1])) /  ((dx_values[i] + dx_values[i - 1]) / 2)
+        k_eff_upper: float = (((dx_values[i] * k_values[i] + dx_values[i + 1] * k_values[i + 1]) * S) / (dx_values[i] + dx_values[i + 1])) /  ((dx_values[i] + dx_values[i + 1]) / 2)
         
         A[i, i - 1], A[i, i + 1] = -k_eff_lower, -k_eff_upper
         A[i, i] = k_eff_lower + k_eff_upper + (p_values[i] * c_values[i] * v_values[i]) / dt
@@ -133,11 +133,11 @@ def solve_mixte_neumann() -> np.ndarray:
             B[i] += src[i] * ((c_values[i] * p_values[i] * v_values[i]) / dt)
 
     # Conditions aux limites
-    k_eff_upper = (((dx_values[0] * k_values[0] + dx_values[1] * k_values[1]) * S) / (dx_values[0] + dx_values[1])) /  ((dx_values[0] + dx_values[1]) / 2)
+    k_eff_upper: float = (((dx_values[0] * k_values[0] + dx_values[1] * k_values[1]) * S) / (dx_values[0] + dx_values[1])) /  ((dx_values[0] + dx_values[1]) / 2)
     A[0, 1] = -k_eff_upper
     A[0, 0], B[0] = h_air * S +  k_eff_upper + ((c_values[0] * p_values[0] * v_values[0]) / dt), h_air * S  * T_left + T_old[0]  
 
-    k_eff_lower = (((dx_values[-1] * k_values[-1] + dx_values[-2] * k_values[-2]) * S) / (dx_values[-1] + dx_values[-2])) /  ((dx_values[-1] + dx_values[-2]) / 2)
+    k_eff_lower: float = (((dx_values[-1] * k_values[-1] + dx_values[-2] * k_values[-2]) * S) / (dx_values[-1] + dx_values[-2])) /  ((dx_values[-1] + dx_values[-2]) / 2)
     A[-1, -2] = -k_eff_lower
     A[-1, -1], B[-1] = k_eff_lower + ((p_values[-1] * c_values[-1] * v_values[-1]) / dt), T_old[-1]
     
@@ -154,14 +154,14 @@ def update_ani(frame):
     T_new = all_temperatures[frame]
     source_status = all_source_status[frame]
     line.set_data(x, T_new)
-    time_text.set_text(f" Heure de simulation : {frame * dt / 3600:.0f}h \n Température intérieur {float(T_new[-1]):.2f}°C\n Status de l'élement chuaffant: {'Activé' if source_status else 'Désactivé'}")
+    time_text.set_text(f" Heure de simulation : {frame * dt / 3600:.0f}h \n Température intérieur {float(T_new[-1]):.2f}°C\n Statut de l'élement chauffant: {'Activé' if source_status else 'Désactivé'}")
     return line, time_text
 
 
 ##########################################
 #           Boucle de simulation         #
 ##########################################
-x = [(dx_values[0] / 2)]
+x: list[float] = [(dx_values[0] / 2)]
 for i in range(NVF_tot - 1):
     x.append(x[-1] + ((dx_values[i] + dx_values[i + 1]) / 2))
 
@@ -169,7 +169,7 @@ for i in range(NVF_tot - 1):
 print("Bonjour bienvenu dans la simulation de Benjamin PELLIEUX Mixte - Neumann")
 print(f"[INFO] Running Mixte - Neumann")
 
-print(f"[INFO] Durée en seconde {t_total}s")
+print(f"[INFO] Durée de la simulation : {(t_total / 3600):.0f}h -> {t_total}s")
 print(f"[INFO] Nombre d'éléments finis : {NVF_tot}")
 print(f"[INFO] Intervalle de temps en secondes {dt}s  {int(dt/60)} minutes")
 
@@ -190,10 +190,10 @@ for _ in range(0, t_total, dt):
 fig, ax = plt.subplots(figsize=(10,10))
 line, = ax.plot([], [], "-", color="red", lw=2, label="Température")
 ax.set_xlim(0, L)
-ax.set_ylim(0, 30)
+ax.set_ylim(0, 35)
 ax.set_xlabel("Position (m)")
 ax.set_ylabel("Température (°C)")
-ax.set_title(f"Évolution de la température dans le mur en fonction du temps\n Temparture cible: {T_cible}°C")
+ax.set_title(f"Évolution de la température dans le mur en fonction du temps \navec un élément chauffant de {power_beton_conducteur}W et une temparture cible: {T_cible}°C")
 ax.legend(loc="upper left")
 
 time_text = ax.text(0.00, 0.85, '', transform=ax.transAxes, color="black", fontsize=12, ha="left")
