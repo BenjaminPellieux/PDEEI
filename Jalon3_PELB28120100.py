0d1b3a4522ba7bed02e8faca1c390f2b38d7f2cc
Benjamin PELLIEUX <benjaminpellieux5@gmail.com>
2024-11-19 22:43:17 -0500

    EDIT code with new value for k_beton

--- Jalon3_PELB28120100.py
+++ Jalon3_PELB28120100.py
@@ -27,7 +27,7 @@ from matplotlib.animation import FuncAnimation
 
 # Animation
 FPS: int = 60  # Fréquence d'images par seconde pour l'animation
-GIF: bool = False  # Si True, enregistre l'animation en GIF
+GIF: bool = True  # Si True, enregistre l'animation en GIF
 
 ###################################
 #        Données géométriques     #
@@ -44,13 +44,13 @@ S: int = h * l  # Surface du mur (m²)
 
 # Températures initiales et limites
 T_init: int = 10  # Température initiale dans le mur (°C)
-T_left: int = 23  # Température à la frontière gauche (°C) - Dirichlet
+T_left: int = -15  # Température à la frontière gauche (°C) - Dirichlet
 T_cible: int = 18 # Température cible pour le mur de droite
 
 # Propriétés des matériaux
 k_beton, c_beton, p_beton = 1.28, 880, 2200  # Béton standard
 k_air, c_air, p_air, h_air = 0.026, 1004, 1.204, 20  # Air
-power_beton_conducteur: int = 200  # Puissance thermique dans la couche chauffante (W)
+power_beton_conducteur: int = 250  # Puissance thermique dans la couche chauffante (W)
 
 ###################################
 #     Parametre de Simulation     #
@@ -93,7 +93,7 @@ dx_values: np.ndarray[float] = np.array(
 v_values = dx_values * S
 
 # Paramètres temporels
-HEURES: int = 48  # Durée de la simulation (heures)
+HEURES: int = 96  # Durée de la simulation (heures)
 t_total: int = 3600 * HEURES  # Simulation sur X heures
 dt: int = 1800  # Intervalle de temps en secondes
 
@@ -188,7 +188,7 @@ for _ in range(0, t_total, dt):
 fig, ax = plt.subplots(figsize=(10,10))
 line, = ax.plot([], [], "-", color="red", lw=2, label="Température")
 ax.set_xlim(0, L)
-ax.set_ylim(-5, 35)
+ax.set_ylim(-15, 35)
 ax.set_xlabel("Position (m)")
 ax.set_ylabel("Température (°C)")
 ax.set_title(f"Évolution de la température dans le mur en fonction du temps \navec un élément chauffant de {power_beton_conducteur}W et une temparture cible: {T_cible}°C")
